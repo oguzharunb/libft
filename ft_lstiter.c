8a078881270b6ad4765784e83ad3ada7340714a2
oguzharunb <oguzharunb@gmail.com>
2024-10-25 13:54:55 +0300

    null check deleted

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: obastug <obastug@42kocaeli.com.tr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:19:42 by obastug           #+#    #+#             */
-/*   Updated: 2024/10/09 13:21:39 by obastug          ###   ########.fr       */
+/*   Updated: 2024/10/25 13:49:05 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (!lst || !f)
-		return ;
 	while (lst)
 	{
 		f(lst->content);
