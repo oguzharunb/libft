0ceee68f5dc35626cfaee16d4372cde904980740
oguzharunb <oguzharunb@gmail.com>
2024-10-25 13:55:23 +0300

    nullcheck deleted

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,18 +6,17 @@
 /*   By: obastug <obastug@42kocaeli.com.tr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:37:06 by obastug           #+#    #+#             */
-/*   Updated: 2024/10/24 15:37:13 by obastug          ###   ########.fr       */
+/*   Updated: 2024/10/25 13:42:43 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+__attribute__((nonnull(1, 2)))
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
-	if (!s || !f)
-		return ;
 	i = 0;
 	while (s[i])
 	{
