f544cd720274a656bc72416790c8f5654a6c4925
oguzharunb <oguzharunb@gmail.com>
2024-10-25 14:10:15 +0300

    rewrote

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: obastug <obastug@42kocaeli.com.tr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:11:02 by obastug           #+#    #+#             */
-/*   Updated: 2024/10/24 15:40:34 by obastug          ###   ########.fr       */
+/*   Updated: 2024/10/25 14:09:16 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,15 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*ptr;
 	char	*ret;
 
-	if (c == 0)
-		return ((char *)s + ft_strlen(s));
-	ret = (void *)0;
-	ptr = (char *)s;
-	while (*ptr)
+	ret = NULL;
+	if (*s == c)
+		ret = (char *)s;
+	while (*s++)
 	{
-		ptr = ft_memchr(ptr, c, ft_strlen(ptr) + 1);
-		if (!ptr)
-			return (ret);
-		ret = ptr;
-		ptr++;
+		if (*s == c)
+			ret = (char *)s;
 	}
-	if (ret)
-		return (ret);
-	return ((void *)0);
+	return (ret);
 }
