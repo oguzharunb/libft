dc57dad1cb69b46c7375926c9b37d419b48b07b1
oguzharunb <oguzharunb@gmail.com>
2024-10-28 11:57:14 +0300

    attribute deleted

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,13 +6,12 @@
 /*   By: obastug <obastug@42kocaeli.com.tr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:18:27 by obastug           #+#    #+#             */
-/*   Updated: 2024/10/09 14:43:51 by obastug          ###   ########.fr       */
+/*   Updated: 2024/10/28 11:57:04 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-__attribute__((nonnull(1, 2)))
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*tmp;
