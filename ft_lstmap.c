a190cd5181902954ea3984264c846e8c50e25ec8
oguzharunb <oguzharunb@gmail.com>
2024-10-28 11:57:33 +0300

    null check extra condition added

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: obastug <obastug@42kocaeli.com.tr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:21:02 by obastug           #+#    #+#             */
-/*   Updated: 2024/10/09 13:21:33 by obastug          ###   ########.fr       */
+/*   Updated: 2024/10/28 11:57:17 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list			*new_list;
 	register t_list	*iter_src;
 	register t_list	*iter_new;
+	void			*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
@@ -24,9 +25,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	iter_src = lst;
 	while (iter_src)
 	{
-		iter_new = ft_lstnew(f(iter_src->content));
+		content = f(iter_src->content);
+		iter_new = ft_lstnew(content);
 		if (!iter_new)
 		{
+			if (content)
+				del(content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
